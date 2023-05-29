ff3516006e2697f5e3ba410753812ec2f711cf01
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-05-29 13:13:45 +0600

    /How o make temperature converter (12)

---
+++ How o make temperature converter (12)/index.js
@@ -0,0 +1,14 @@
+/*
+var farn = parseFloat(prompt("Enter Fahrenheight"));
+
+var Cels = (farn-32) * (5/9);
+
+document.write("Celcius =" + Cels); */
+
+
+
+var cels = parseFloat(prompt("Enter Celcius"));
+
+var farn = (cels * (9 / 5)) + 32;
+
+document.write("Farenheight =" + farn); 
