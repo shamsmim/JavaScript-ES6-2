21cec2ed365f30a501e8a4db4e53e04533aa4ce5
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-05-30 21:26:50 +0600

    Task-4

---
+++ Task-4/index.js
@@ -0,0 +1,16 @@
+var num = prompt("Enter your number :")
+
+if(num>=80)
+    console.log("A+");
+else if(num>=70)
+    console.log("A");
+else if(num>=60)
+    console.log("A-");
+else if(num>=50)
+    console.log("B");
+else if(num>=40)
+    console.log("C");
+else if(num>=33)
+    console.log("D");
+else
+    console.log("Fail");
