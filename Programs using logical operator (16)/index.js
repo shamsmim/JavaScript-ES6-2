0f18e2c7f0779c7e852cfb7c31a927889a042a9a
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-06-06 12:15:30 +0600

    Programs using logical operator (16)

---
+++ Programs using logical operator (16)/index.js
@@ -0,0 +1,12 @@
+ var num1 = prompt("Enter num1 : ");
+ var num2 = prompt("Enter num2 : ");
+ var num3 = prompt("Enter num3 : ");
+
+ if(num1>num2 && num1>num3)
+    console.log("Large number =" +num1);
+
+    if(num2>num1 && num2>num3)
+    console.log("Large number =" +num2);
+
+    else
+    console.log("Large number =" +num3);
