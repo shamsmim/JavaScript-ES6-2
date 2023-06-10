2aabf43db782faba403df41a47d9e9abe83c435e
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-06-11 01:02:36 +0600

    Ternary operator (23)

---
+++ Ternary operator (23)/TERNARY_CONDITION.js
@@ -0,0 +1,14 @@
+ var number = Number(prompt("Enter a number"));
+/* if(number>0){
+    console.log("Positive");
+}
+
+else{
+    
+        console.log("Negative");
+    }
+*/
+
+  var result = number > 0 ? "Positive" : "Negative"
+
+  console.log(result);
