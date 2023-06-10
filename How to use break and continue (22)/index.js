444f2e607de461077c63a7517a96c0aa5f084dee
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-06-11 00:26:57 +0600

    How to use break and continue (22)

---
+++ How to use break and continue (22)/index.js
@@ -0,0 +1,26 @@
+ /*
+ for(var i=1; i<=100; i++){
+    
+    document.write(" "+i);
+
+    if (i==10){
+        break;
+    }
+ }
+
+ document.write(" end"); */
+
+ for(var i=1; i<=100; i++){
+    
+    if (i==10){
+        continue;
+    }
+    document.write(" "+i);
+
+    
+ }
+
+ document.write(" end");
+
+ 
+
