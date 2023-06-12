08e5ce35907b6a3164b22aac26f1399caf2c8af7
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-06-12 22:12:36 +0600

    How to create and using array (26)

---
+++ How to create and using array (26)/index.js
@@ -0,0 +1,22 @@
+var name1, name2, name3, name4, name5;
+name1 = "Anis"
+name2 = "Islam"
+name3 = "Shmas"
+name4 = "Mim"
+name5 = "Dell"
+
+// console.log(name5);
+
+var names = ["Anis", "Islam", "Shams", "Mim", "Dell"];
+// console.log(names);
+// console.log(names[3]);
+
+names.push("DCL")
+
+console.log(names.length);
+
+var country1 = ["Bangladesh", "India"];
+var country2 = ["Pakistan", "Nepal"]
+
+var country = country1.concat(country2);
+console.log(country);
