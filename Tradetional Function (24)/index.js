64113c377da3a978938b27dcf8ffd7b9cb05ea05
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-06-12 12:41:00 +0600

    Tradetional Function (24)

---
+++ Tradetional Function (24)/index.js
@@ -0,0 +1,32 @@
+
+
+//Creating a function 
+function square(num1, num2)
+{
+    //var num = 5;
+    var result = num1 * num2;
+    document.write("Result = "+ result + "</br>")
+}
+
+// Calling a function
+
+square(5, 8);
+
+
+
+
+
+
+
+
+
+
+
+/*
+var num = 6;
+var result = num * num;
+document.write("Result = "+ result + "</br>")
+
+var num = 7;
+var result = num * num;
+document.write("Result = "+ result + "</br>") */ 
