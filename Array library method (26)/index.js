43a828f9ca70d826c594791e3f2f2a7f8477e1cb
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-06-14 23:16:06 +0600

    Array library method (26)

---
+++ Array library method (26)/index.js
@@ -0,0 +1,14 @@
+// array methods
+// shift(), unshift(), splice(pos,neo,ele1,ele2..), sp..
+
+var names = ["Anis", "Islam", "Shams"];
+
+console.log(names);
+
+//shift opposite of pop
+names.shift();
+console.log(names);
+
+// unshift opposite of push
+names.unshift("Mahi");
+console.log(names);
