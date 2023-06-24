96e4d842dc24ada84a2a64c9a1abea31da50980d
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-06-24 23:56:08 +0600

    How to create multiple object (31) part-3 class name

---
+++ How to create multiple object (31) part-3 get element by class name/index.js
@@ -0,0 +1,21 @@
+// getElementsByTagName 
+
+// var myheading = document.getElementById("heading1");
+// myheading.innerHTML = "Hello"
+
+// document.getElementById("para").innerHTML = "Bye ";
+
+// var myheading2 = document.getElementById("heading2")
+
+// myheading2.innerHTML = "Good Bye Good Bye";
+
+
+
+
+
+// document.getElementsByTagName("h1")[0].innerHTML = "HI"
+
+
+// Get element by class name
+
+document.getElementsByClassName("para1")[0].innerHTML = "HI";
