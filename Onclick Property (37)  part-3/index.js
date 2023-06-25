2811e4ccafbd4111ac40b074c0e3f0d877bdfaff
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-06-26 00:25:25 +0600

    Onclick Property (37) part-3

---
+++ Onclick Property (37)  part-3/index.js
@@ -0,0 +1,17 @@
+var myVar = document.querySelector("#myImgID");
+
+function myPicture1(){
+
+    
+    myVar.src = "images/image 1.jpg";
+}
+
+function myPicture2(){
+
+    
+    myVar.src = "images/image 2.jpg";
+}
+
+
+
+
