2b17caa008a5c850f342882f052f863d04173f18
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-06-26 00:24:21 +0600

    Onclick Property (37) part-2

---
+++ Onclick Property (37)  part-2/index.js
@@ -0,0 +1,10 @@
+function myMessage1(){
+    var myVar = document.querySelector("#para1");
+    myVar.innerHTML = "you have click on button 1";
+}
+
+function myMessage2(){
+    var myVar = document.querySelector("#para1");
+    myVar.innerHTML = "you have click on button 2";
+}
+
