982d1cd77fb6595563e5bf7a6c14d95196155f56
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-10 23:39:49 +0600

    /Event Listener with multiple elements (43) part-2

---
+++ Event Listener with multiple elements (43) part-2/index.js
@@ -0,0 +1,21 @@
+
+for(var i = 0; i < 3; i ++){
+
+    document.querySelectorAll(".myButton")[i].addEventListener("click", function(){
+
+        var text = this.innerHTML;
+        document.querySelector("h1").innerHTML = text + " is clicked";
+    
+    });
+    
+
+}
+
+
+
+
+
+
+
+
+
