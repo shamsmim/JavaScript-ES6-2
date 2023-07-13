f94dd0d5490b3aa054a803f0ca03c873f510da6b
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-13 23:58:21 +0600

    how to add animation (45)

---
+++ Keypress Listener (46)/index.js
@@ -0,0 +1,62 @@
+
+    
+for(var i = 0; i <3; i++){
+
+    document.querySelectorAll(".myButton")[i].addEventListener("click",function(){
+        var text = this.innerHTML;
+        console.log(text);
+        audioPlay(text);
+        playAnimation(text);
+})
+
+}
+
+
+
+
+document.addEventListener("keypress",function(event){
+  var text = event.key;
+  audioPlay(text);
+  playAnimation(text);
+});
+
+
+
+
+
+
+
+
+
+
+function audioPlay(text){
+
+    switch (text) {
+        case "a":
+            var audio = new Audio("audio/a.mp3");
+            audio.play();
+            break;
+    
+            case "b":
+                var audio = new Audio("audio/b.mp3");
+                audio.play();
+                break;
+    
+                case "c":
+                var audio = new Audio("audio/c.mp3");
+                audio.play();
+                break;
+     }
+}
+
+
+ function playAnimation(text){
+
+    var slecetedButton = document.querySelector("."+text)
+    slecetedButton.classList.add("anim")
+
+
+    setTimeout(function() {
+        slecetedButton.classList.remove("anim")
+    }, 1000);
+ }
