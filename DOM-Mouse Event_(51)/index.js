a289c164fb1e3a9ef6fdaa30e5ffb9cf7b88c2b8
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-18 23:41:39 +0600

    DOM-Mouse Event_(51)

---
+++ DOM-Mouse Event_(51)/index.js
@@ -0,0 +1,13 @@
+console.clear();
+
+const div = document.querySelector("div");
+
+div.addEventListener("click", function(){
+
+    console.log("click is occured");
+})
+
+div.addEventListener("dblclick", function(){
+
+    console.log("dblclick is occured");
+})
