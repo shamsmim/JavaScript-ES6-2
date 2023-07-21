e08086c18d8d450db845292ef940e058da33e1dd
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-21 23:17:48 +0600

    DOM-Focus-Event (53)

---
+++ DOM-Focus-Event (53)/index.js
@@ -0,0 +1,18 @@
+
+
+const input = document.querySelector('input');
+input.addEventListener('blur', function(){
+    input.style.backgroundColor = "transparent";
+    input.style.padding = "0";
+})
+input.addEventListener('focus', function(){
+    // console.log('focus is occured');
+
+    //  
+})
+// input.addEventListener('focusin', function(){
+//     console.log('focusin is occured');
+// })
+// input.addEventListener('focusout', function(){
+//     console.log('focusout is occured');
+// })
