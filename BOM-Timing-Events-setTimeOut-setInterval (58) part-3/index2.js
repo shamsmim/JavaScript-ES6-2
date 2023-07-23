cbf6c312948007a502d0f5cfa94fb560e2e1cce8
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-24 00:48:28 +0600

    BOM-Timing-setTimeOut-setInterval (58) part-3

---
+++ BOM-Timing-Events-setTimeOut-setInterval (58) part-3/index2.js
@@ -0,0 +1,33 @@
+// js BOM
+// js Timing Event
+// stTimeOut (), setInterval () 
+
+// setTimeout(() => {
+//     console.log("Hi");
+// }, 2000);
+
+// setTimeout(display, 2000);
+// function display(){
+//     console.log("display function");
+// }
+
+
+
+//............setTimeout...........
+
+
+// const saveButton = document.querySelector(".save-btn");
+// const message = document.querySelector(".message");
+
+// saveButton.addEventListener('click',saveUser);
+
+// function saveUser(){
+//     message.textContent = "user registration successful";
+
+//     setTimeout(() => {
+//     message.textContent = "";
+//     }, 2000);
+// }
+
+
+
