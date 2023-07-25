46cd925ba6b41d33b0fdcac74d5da492975d9c2e
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-25 22:04:35 +0600

    Error Handing try, catch, finally block (61)

---
+++ Error Handing try, catch, finally block (61)/index.js
@@ -0,0 +1,15 @@
+  try{
+    // code text
+    alert("Hi Everyone");
+    alert(x);
+    alert("Bye everyone");
+
+  }catch(err){
+    //handeling
+    console.log("Inside catch");
+    console.log(err);
+  }
+
+  //alert("Hi Everyone"); 
+  //alert(x);
+  //alert("Bye Everyone")
