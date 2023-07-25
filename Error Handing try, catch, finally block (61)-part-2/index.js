21bd1ffaf9e6df62a535c70dac4a9c787bd3aa3f
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-25 22:05:24 +0600

    Error Handing try-catch, finally block (61)-part-2

---
+++ Error Handing try, catch, finally block (61)-part-2/index.js
@@ -0,0 +1,18 @@
+  try{
+    // code text
+    alert("Hi Everyone");
+    alert(x);
+    
+
+  }catch(err){
+    
+     console.log(err);
+    // console.log(err.name);
+    // console.log(err.message);
+  }finally{
+    alert("Bye everyone");
+  }
+
+  //alert("Hi Everyone"); 
+  //alert(x);
+  //alert("Bye Everyone")
