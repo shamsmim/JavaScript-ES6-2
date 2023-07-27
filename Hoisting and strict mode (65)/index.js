4106525a148b19baca45dce9452321e201657fb0
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-27 21:03:23 +0600

    Hoisting and strict mode (65)

---
+++ Hoisting and strict mode (65)/index.js
@@ -0,0 +1,15 @@
+
+// x=20;
+// if(true){
+//     y=10;
+//     console.log(`y = ${y}`);
+//     var y;
+// }
+// console.log(`x = ${x}`);
+// var x;
+
+//..........Use Strict..........
+
+"use strict"
+x=20;
+console.log(`x = ${x}`);
