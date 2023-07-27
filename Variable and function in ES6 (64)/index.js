3d1671dfc25fee47567bb6b28c38a83603a43ddc
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-27 21:02:57 +0600

    Variable and function in ES6 (64)

---
+++ Variable and function in ES6 (64)/index.js
@@ -0,0 +1,21 @@
+//.........ES6 Variable Dicleration..........
+
+let x = 25;
+let y = 30;
+let sum = x+y;
+console.log(`sum is ${sum}`);
+
+//........part-2.............
+
+let name = "Ash Shams Mim";
+
+let message = `I am ${name} I am learning javaScript`;
+console.log(message); 
+
+//.........ES6 Function Dicleration.....
+
+const add = (x,y) => {
+    let sum = x+y;
+    console.log(sum);
+}
+add (30,20);
