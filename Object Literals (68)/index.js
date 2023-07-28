445e9c1d208261ae89de087bce0cb7d9a408e3bd
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-28 22:14:36 +0600

    Object Literals (68)

---
+++ Object Literals (68)/index.js
@@ -0,0 +1,23 @@
+// Boject Literals
+
+// function studentinfo (name, age){
+//     return{
+//         name,
+//         age
+//     }
+// }
+// console.log(studentinfo("Shams", 20));
+
+// let message = {
+//     body (){
+//         return `Hi, I am object function`
+//     }
+// }
+// console.log(message.body());
+
+let message = {
+    'body name'(){
+        return `Hi, I am object function`
+    }
+}
+console.log(message['body name']());
