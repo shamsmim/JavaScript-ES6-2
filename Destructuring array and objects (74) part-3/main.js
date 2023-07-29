9b99f067b6dd1e3e6588a3d46812105089d0f255
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-07-30 03:20:00 +0600

    Destructuring array and objects (74) part-3

---
+++ Destructuring array and objects (74) part-3/main.js
@@ -0,0 +1,10 @@
+// object destructure
+
+ const studentInfo = {
+     id : 101,
+     fullName : 'Shams Mim',
+     gpa : 3.81
+
+ }
+ const {id, fullName} = studentInfo
+ console.log(id, fullName);
