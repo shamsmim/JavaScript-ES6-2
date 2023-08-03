45eff22068213f1907d50ae63b5bbdd4445b70d7
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-03 22:19:46 +0600

    (76) - String methods _ startsWith, endsWith, includes

---
+++ (76) - String methods _ startsWith, endsWith, includes/main.js
@@ -0,0 +1,14 @@
+// startsWith(SearchString, position) -> check a string starts with another string
+
+// const message = "Today is friday";
+// console.log(message.startsWith('Today'));
+
+// endsWith (searchString, length) -> check a string ends with another string
+
+const message = "Today is friday";
+console.log(message.endsWith('firday'));
+// includes(searchString, position) -> check if a string contains another string
+
+// const message = "Today is friday";
+// console.log(message.includes('is'));
+// all these are case sensitive
