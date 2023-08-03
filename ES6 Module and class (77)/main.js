4be64a0c6fd59ade77a52b551bda4b17013e8d30
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-03 22:20:07 +0600

    /ES6 Module and class (77)

---
+++ ES6 Module and class (77)/main.js
@@ -0,0 +1,5 @@
+import {text, setText} from './myModule.js'
+
+console.log(text);
+setText("GoodBye from ES6");
+console.log(text);
---
+++ ES6 Module and class (77)/myModule.js
@@ -0,0 +1,5 @@
+export let text = "welcome to myModule";
+
+export function setText(txt){
+    text = txt;
+}
