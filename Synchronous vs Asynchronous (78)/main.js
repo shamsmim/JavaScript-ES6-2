160a2cd1a3389a3857c5fbee8a7c1ad0b705a98c
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-04 22:27:09 +0600

    Synchronous vs Asynchronous (78)

---
+++ Synchronous vs Asynchronous (78)/main.js
@@ -0,0 +1,31 @@
+// Synchronous Function
+
+const taskOne = () => {
+    console.log("Task1");
+}
+
+const dataLoading = () =>{
+    console.log("Task2 .dataLoading");
+}
+
+const taskTwo = () => {
+    setTimeout(dataLoading, 2000)
+}
+
+const taskThree = () => {
+    console.log("Task3");
+}
+
+const taskFour = () => {
+    console.log("Task4");
+}
+
+const taskFive = () => {
+    console.log("Task5");
+}
+
+taskOne();
+taskTwo();
+taskThree();
+taskFour();
+taskFive();
