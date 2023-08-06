cb88c51c86476364a40b1535326984ad67cba375
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-06 21:35:45 +0600

    Async await javascript (82)

---
+++ Async await javascript (82)/main.js
@@ -0,0 +1,53 @@
+console.log("Welcome");
+
+const taskOne = () => {
+    return new Promise((resolve, reject) => {
+        resolve("task 1 is completed")
+    })
+}
+const taskTwo = () => {
+    return new Promise((resolve, reject) => {
+        
+        setTimeout(() => {
+            reject("task 2 is not completed")
+        }, 2000)
+    })
+}
+const taskThree = () => {
+    return new Promise((resolve, reject) => {
+        resolve("task 3 is completed")
+    })
+}
+const taskFour = () => {
+    return new Promise((resolve, reject) => {
+        resolve("task 4 is completed")
+    })
+}
+
+// taskOne().then((res) =>  console.log(res))
+// .then(taskTwo)
+// .then((res) => console.log(res))
+// .then(taskThree)
+// .then((res) => console.log(res))
+// .then(taskFour)
+// .then((res) => console.log(res))
+// .catch((err) => console.log(err))
+
+ const callAllTasks = async () => {
+    try{
+        const t1 = await taskOne();
+    console.log(t1);
+    const t2 = await taskTwo();
+    console.log(t2);
+    const t3 = await taskThree();
+    console.log(t3);
+    const t4 = await taskFour();
+    console.log(t4);
+    }catch(e){
+        console.log(e);;
+    }
+}
+
+callAllTasks();
+
+console.log("Bye");
