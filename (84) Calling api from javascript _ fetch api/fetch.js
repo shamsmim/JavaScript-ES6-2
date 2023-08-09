3b89a08fab9cadb93f4b446d2e0414a922f5efda
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-10 00:20:37 +0600

    (84) Calling api from javascript _ fetch api

---
+++ (84) Calling api from javascript _ fetch api/fetch.js
@@ -0,0 +1,21 @@
+// 4 way to call api--XMLHttpRequest fetch, axios, jquery
+// fetch() - global method for making Http-Request
+// 2 way to call fetch - then, async await
+
+// + fetch() is easy to use compare to XMLHttpRequest
+// + fetch returns a promise
+// - returned promise can only handel network error
+// - dose not support all the older browser
+
+console.clear();
+fetch("https://jsonplaceholder.typicode.com/posts").then((res) => {
+    if(!res.ok){
+        const message = `Error : ${res.status}`;
+        throw new Error(message);
+    }
+    return res.json();
+})
+.then((res) => console.log(res))
+// Error Handeling just use Catch
+.catch((err) => console.log(err));
+
