6ece9f3c0dda82ccdd4996deb70a2c3e8f064934
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-11 21:39:22 +0600

    (85) Calling api from javascript _ axios api- Async Await- PART-3

---
+++ (85) Calling api from javascript _ axios api- Async Await- PART-3/axios.js
@@ -0,0 +1,31 @@
+
+
+console.clear();
+
+const makeRequest = async (config) => {
+    return await axios(config);
+};
+
+const createData = () => {
+    makeRequest({
+        url: 'https://jsonplaceholder.typicode.com/posts',
+        method: 'post',
+        data: JSON.stringify({
+            title: 'foo',
+            body: 'bar',
+            userId: 1,
+          }),
+    })
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+};
+
+createData();
+
+// const getData = () => {
+//     makeRequest("https://jsonplaceholder.typicode.com/posts/1")
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log(err));
+// };
+
+// getData();
