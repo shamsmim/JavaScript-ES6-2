b97cb184b69094b489f8ad2883d6a40641d02bb5
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-16 00:39:16 +0600

    (87)Web Storage API _ localStorage, sessionStorage PART-7

---
+++ (87)Web Storage API _ localStorage, sessionStorage PART-7/local-storage.js
@@ -0,0 +1,19 @@
+// web-storage API - allows us to store & read data in browser
+// web storage API - localstorage, sessionStorage
+
+//localStorage - store, read, update and remove data
+// no expiry data: data never gets lost even if you close the browser
+
+
+
+
+//..........setItem(key, value)......................
+
+ const countries = ["Australia", "Bangladesh", "Nepal", "India"] 
+ localStorage.setItem("countries", JSON.stringify(countries));
+
+//..........setItem(key, value)......................
+
+
+
+
