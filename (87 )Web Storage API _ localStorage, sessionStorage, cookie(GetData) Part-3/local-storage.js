dcb67cfa8be061d020eb999bdc64258d679e96f4
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-15 22:22:13 +0600

    (87 )Web Storage API _ localStorage, sessionStorage, cookie(GetData) Part-3

---
+++ (87 )Web Storage API _ localStorage, sessionStorage, cookie(GetData) Part-3/local-storage.js
@@ -0,0 +1,14 @@
+// web-storage API - allows us to store & read data in browser
+// web storage API - localstorage, sessionStorage
+
+//localStorage - store, read, update and remove data
+// no expiry data: data never gets lost even if you close the browser
+
+
+//..........getItem......................
+
+const userName = localStorage.getItem("userName");
+const userPassword = localStorage.getItem("password");
+console.log(userName, userPassword);
+
+//..........getItem......................
