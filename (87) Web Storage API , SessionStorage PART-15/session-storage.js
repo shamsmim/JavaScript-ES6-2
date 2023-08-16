143119232d56548c5127cc4273c8d29534831cdb
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-16 20:27:24 +0600

    (87) Web Storage API , SessionStorage PART-15

---
+++ (87) Web Storage API , SessionStorage PART-15/session-storage.js
@@ -0,0 +1,16 @@
+// localStorage vs sessonStorage
+// 10mb vs 5mb
+// permanent vs session (tab)
+
+
+
+//..................NEXT............PART..............
+
+const user = {id : "110", name : "Shams"};
+sessionStorage.setItem("user", JSON.stringify(user));
+
+const userInfo = JSON.parse(sessionStorage.getItem("user"));
+console.log(userInfo);
+
+
+
