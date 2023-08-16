1945c52fcffe142a7a7a07f41ab6e55b8315ea1c
Ash Shams Mim <119163073+shamsmim@users.noreply.github.com>
2023-08-16 20:26:48 +0600

    (87) Web Storage API , SessionStorage PART-13

---
+++ (87) Web Storage API , SessionStorage PART-13/session-storage.js
@@ -0,0 +1,16 @@
+// localStorage vs sessonStorage
+// 10mb vs 5mb
+// permanent vs session (tab)
+
+//  sessionStorage.setItem("user1", "Shams");
+//  sessionStorage.setItem("user2", "Mim");
+
+
+
+
+// //sessionStorage.removeItem("user")
+//  sessionStorage.clear();
+
+
+
+
